1cb3f0b99564afda237afe000694b815391bd3d7
Dominykas Osipovičius <domasvz1@gmail.com>
2020-01-29 19:38:39 +0200

    Fixed Item Information Page!
    Fixed Items information page and made it scalable
    
    trying to use as less /br as possible
    
    Now it even looks good!
    
    Need to work o n Modify Items further on!

--- BusinessObjects/Item.cs
+++ BusinessObjects/Item.cs
@@ -19,31 +19,31 @@ namespace BusinessObjects
         public int Id { get; set; }
 
         [Required]
-        [DisplayName("Item Name")]
+        [DisplayName("Item Name: ")]
         public string Name { get; set; }
 
         [Required]
-        [DisplayName("Headline")]
+        [DisplayName("Headline: ")]
         [DataType(DataType.MultilineText)]
         public string Headline { get; set; }
 
         [Required]
-        [DisplayName("Item SKU")]
+        [DisplayName("Item SKU: ")]
         public string SKUCode { get; set; }
 
         //TODO maybe should be better to change from int to unsigned int?
         [Range(1, int.MaxValue, ErrorMessage = "Item price must be positive!")]
-        [DisplayName("Item Price")]
+        [DisplayName("Item Price: ")]
         public int Price { get; set; }
 
-        [DisplayName("Description")]
+        [DisplayName("Description: ")]
         [DataType(DataType.MultilineText)]
         public string Description { get; set; }
 
-        [DisplayName("Kategorijos Id")]
+        [DisplayName("category Id: ")]
         public int? CategoryId { get; set; }
 
-        [DisplayName("Kategorija")]
+        [DisplayName("Category: ")]
         public virtual Category Category { get; set; }
 
         [NotMapped]
